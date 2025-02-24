99c3e0ba9477fe77a5c81c46a5ab10427afba600
Satyam Bheel <136482276+satyam-bheel1999@users.noreply.github.com>
2025-02-24 17:56:04 +0530

    update home

--- src/Components/Navigation/Home.jsx
+++ src/Components/Navigation/Home.jsx
@@ -1,19 +1,55 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import Projects from './Projects';
+
 
 function Home() {
+  const text = "Hi I am Satyam Bheel...."; // Your text to type
+  const [displayText, setDisplayText] = useState("");
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < text.length) {
+      const timeout = setTimeout(() => {
+        setDisplayText((prev) => prev + text[index]);
+        setIndex(index + 1);
+      }, 200); // Typing speed (adjust as needed)
+      return () => clearTimeout(timeout);
+    }
+  }, [index, text]);
+
   return (
-    <div className='h-screen w-screen text-white flex flex-row
-    justify-around relative top-12'>
-      <div className='h-52'>
-        <p className='text-2xl font-bold'>Hi I am <span className='text-green-500'>Satyam Bheel</span> </p> <br />
-        <p>Front-End Developer</p> <br />
+    <>
+    <div className="h-screen w-screen text-white flex flex-row justify-around relative top-12">
+      <div className="h-52 w-2xl">
+        {/* Typing Effect in p tag */}
+        <p className="text-2xl font-bold text-emerald-500">
+          {displayText}
+          <span className="animate-blink">|</span>
+        </p>
+        <p>Front-End Developer</p>
         <p>(React Js, Tailwind Css)</p>
       </div>
       <div>
-        <img src='src/Images/myimg.png'></img>
+        <img src="src/Images/myimg.png" className="rounded-2xl" alt="My Image" />
       </div>
+      <style>
+        {`
+          @keyframes blink {
+            50% { opacity: 0; }
+          }
+          .animate-blink {
+            animation: blink 0.8s infinite;
+          }
+        `}
+      </style>
     </div>
-  )
+
+    <Projects/>
+
+    </>
+
+  );
 }
 
 export default Home
--- src/Components/Navigation/Projects.jsx
+++ src/Components/Navigation/Projects.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Projects() {
   return (
-    <div>
-      
+    <div className='text-white'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda maxime repellendus corrupti expedita beatae non possimus nesciunt dolorum commodi animi, rem quisquam asperiores soluta inventore quae id facilis consequatur. Placeat.
+      Explicabo corrupti dolorem error repellat minus fugiat, omnis quisquam voluptates suscipit natus? Porro corporis unde illo voluptas quos, dolore aut dolor laudantium mollitia, deleniti repudiandae iusto sapiente obcaecati cupiditate placeat.
+      Dignissimos sapiente aliquam, facilis eveniet adipisci nihil fugiat atque voluptates aperiam dolorum obcaecati et incidunt, voluptatem maiores provident temporibus nemo iusto deleniti neque, in laborum earum debitis accusantium recusandae? Officia!
+      Temporibus exercitationem nisi tenetur eum eius. Veritatis distinctio molestiae ullam nulla quaerat repudiandae, pariatur quisquam illo inventore deserunt obcaecati ratione quidem, velit delectus est temporibus minima illum at fuga voluptatem!
+      Explicabo aliquam iste sequi sit exercitationem suscipit recusandae, eos repellendus cumque alias? Obcaecati error totam laboriosam vitae iure perspiciatis animi saepe voluptates tempora expedita aliquid, cupiditate repudiandae ipsa mollitia quod?
+      Fuga id temporibus, velit beatae aliquid eum similique accusantium doloribus perferendis aspernatur quaerat vitae et, voluptate perspiciatis rerum! Sint iusto ab odit rem necessitatibus, eius magni aperiam pariatur autem dolorum!
+      Nesciunt aspernatur dolore sed obcaecati aut magni recusandae, reprehenderit, quaerat iusto quibusdam est, quisquam provident dolorem? Dolorem nesciunt et quod modi quibusdam! Quia assumenda, voluptatibus voluptas expedita dolorum tempora veritatis!
+      Ullam praesentium necessitatibus doloremque neque eius, asperiores repudiandae beatae minus cupiditate quas voluptatum explicabo eum aperiam cumque magni blanditiis laborum nulla perspiciatis sit corrupti saepe. Nisi facere tenetur quaerat autem.
+      Beatae quaerat, accusamus quidem minima nam adipisci nemo animi eum atque tempora dolorum eveniet asperiores assumenda exercitationem vel! Eveniet vel fuga ex modi commodi suscipit alias saepe officia iusto inventore!
+      Esse inventore incidunt ullam molestiae dolores, est ratione. Esse nobis sapiente quos repellendus assumenda, maxime et qui quisquam, hic voluptatum saepe ipsum facilis laborum corrupti vitae sit veritatis, illum unde.
     </div>
   )
 }
